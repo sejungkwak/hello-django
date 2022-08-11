b9758217bdd6523f8d16a2c95cdec8035866f82d
Sejung Kwak <kwak.sejung@gmail.com>
2022-08-11 20:00:01 +0000

    Complete walkthrough

---
+++ todo/test_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        item_id = item.id
+        response = self.client.get(f'/edit/{item_id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
+
+    def test_can_add_item(self):
+        response = self.client.post('/add', {'name': 'Test Added Item'})
+        self.assertRedirects(response, '/')
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        item_id = item.id
+        response = self.client.get(f'/delete/{item_id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.objects.filter(id=item_id)
+        self.assertEqual(len(existing_items), 0)
+
+    def test_can_toggle_item(self):
+        item = Item.objects.create(name='Test Todo Item', done=True)
+        item_id = item.id
+        response = self.client.get(f'/toggle/{item_id}')
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item_id)
+        self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        item_id = item.id
+        response = self.client.post(f'/edit/{item_id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item_id)
+        self.assertEqual(updated_item.name, 'Updated Name')
